710332f163523f6bf15b6018faecc430577a23cc
Miguel Paolo <mpbravo35@yahoo.com>
2018-01-16 02:57:45 -0800

    1

---
+++ AppTester/TestCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  TestCollectionViewCell.swift
+//  AppTester
+//
+//  Created by Miguel Paolo Bravo on 1/16/18.
+//  Copyright © 2018 Miguel Paolo Bravo. All rights reserved.
+//
+
+import UIKit
+
+class TestCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var labelTest: UILabel!
+}
